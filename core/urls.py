75ccb236ad9960586f9717d109045c69398c7aa3
yisuscoin <r.alfaro1882@gmail.com>
2022-12-24 13:41:13 -0500

    guardando cambios

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from .views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #se ha reescrito la vista anterior
+    path('', HomeView.as_view(), name="home")
 ]
