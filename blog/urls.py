adfe55cbb4e6c4c88b49c8ad5be4dacf23dd0267
yisuscoin <r.alfaro1882@gmail.com>
2022-12-25 09:55:25 -0500

    enlace a blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 #creando un url basico para poder acceder a todas las vistas que estamos por crear de nuestro blog
 #y estamos conectando al url de core
 from django.urls import path
+from .views import BlogListView
 
 app_name="blog"
 
 urlpatterns = [
-    
+    path('', BlogListView.as_view(), name="home")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
-#creando el indice donde vamos a enlistar todos los post que existen
+#creando la vista del indice donde vamos a enlistar todos los post que existen
+
+class BlogListView(View):
+    def get(self, request, *args, **kwargs):
+        context={
+            
+        }
+        return render(request,'blog_list.html', context)
