11b487be6d6a6c52cffb18dc11a45c4653f70b25
yisuscoin <r.alfaro1882@gmail.com>
2022-12-27 15:09:41 -0500

    form

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_objetc_or_404, render, redirect
 from django.views.generic import View
 #importando el formulario creado
 from .forms import PostCreateForm
+from .models import Post
 
 # Create your views here.
 #creando la vista del indice donde vamos a enlistar todos los post que existen
 
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
+        
         context={
             
         }
@@ -17,12 +19,22 @@ class BlogListView(View):
 
 class BlogCreateView(View):
     def get(self, request, *args, **kwargs):
+        form=PostCreateForm()
         context={
-            
+            'form':form
         }
         return render(request, 'blog_create.html', context)
     
     def post(self, request, *args, **kwargs):
+        if request.method=="POST":
+                form = PostCreateForm(request.POST)
+                if form.is_valid():
+                    title = form.cleaned_data.get('title')
+                    content = form.cleaned_data.get('content')
+                    
+                    p, created = Post.objects.get_or_create(title=title, content=content)
+                    p.save()
+                    return redirect('blog:home')
         context={
             
         }
